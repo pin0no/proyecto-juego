49d3646560a5cfa1bd7f081472d2001240b632ab
bruno martinez flores <bruno.marflo@gmail.com>
2022-07-20 18:12:18 -0300

    inclusion estructura

--- Proyecto prog2/main.cpp
+++ Proyecto prog2/main.cpp
@@ -6,30 +6,41 @@
 #define ancho 1024
 #define alto 768
 
+struct posicion
+{
+	int posicionX = 0;
+	int posicionY = 0;
+};
+typedef posicion movimiento;
 using namespace std;
 
-int posicionX = 0/*512*/, posicionY = 0/*384*/;
+/*int posicionX = 0 , posicionY = 0;*/
+
 
 ALLEGRO_EVENT_QUEUE* event_queue;
 
 int main()
 {
-	al_init();
+	movimiento jugador; /*movimiento jugador en posiciones X e Y*/
+
+
+
+	al_init();			/*iniciaciones*/
 	al_install_keyboard();
 	al_init_image_addon();
 	
-	ALLEGRO_DISPLAY* ventana = al_create_display(ancho, alto);
+	ALLEGRO_DISPLAY* ventana = al_create_display(ancho, alto);/*crear una ventana*/
 
-	int ANCHO_W = GetSystemMetrics(SM_CXSCREEN);
+	int ANCHO_W = GetSystemMetrics(SM_CXSCREEN);/*obtener los pixeles de la pantalla que se utiliza*/
 	int ALTO_W = GetSystemMetrics(SM_CYSCREEN);
 
-	al_set_window_title(ventana, "primer avance");
-	al_set_window_position(ventana, ANCHO_W/2-ancho/2, ALTO_W/2-alto/2);
+	al_set_window_title(ventana, "primer avance");/*cambio de titulo*/
+	al_set_window_position(ventana, ANCHO_W/2-ancho/2, ALTO_W/2-alto/2);/*centrar la ventana*/
 
 	event_queue = al_create_event_queue();
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
 
-	ALLEGRO_BITMAP* menu_null = al_load_bitmap("imagenes/cesped.jpg");
+	ALLEGRO_BITMAP* menu_null = al_load_bitmap("imagenes/cesped.jpg");/*imagenes a utilizar*/
 	ALLEGRO_BITMAP* menu_null1 = al_load_bitmap("imagenes/hola.png");
 	/*ALLEGRO_BITMAP* menu_null1 = al_load_bitmap("imagenes/cara_benja.PNG"); */
 	
@@ -39,34 +50,34 @@ int main()
 	{
 		ALLEGRO_EVENT evento;
 		/*al_clear_to_color(al_map_rgb(0, 0, 0));*/
-		al_draw_bitmap(menu_null, 0, 0, 0);
-		al_draw_bitmap(menu_null1, posicionX, posicionY, 0);
-		al_flip_display();
+		al_draw_bitmap(menu_null, 0, 0, 0);/*primera imagen*/
+		al_draw_bitmap(menu_null1, jugador.posicionX, jugador.posicionY, 0);/*segunda imagen*/
+		al_flip_display();/*mostrar las imagenes en pantalla*/
 		al_wait_for_event(event_queue, &evento);
 		
 
 		if (evento.type == ALLEGRO_EVENT_KEY_DOWN)
 		{
-			switch (evento.keyboard.keycode)
+			switch (evento.keyboard.keycode)/*hacer una accion para cada tecla pulsada*/
 				{
 				case ALLEGRO_KEY_D:
 				{
-					posicionX = posicionX + 30;	
+					jugador.posicionX = jugador.posicionX + 30;
 					break;
 				}
 				case ALLEGRO_KEY_A: 
 				{
-					posicionX = posicionX - 30;
+					jugador.posicionX = jugador.posicionX - 30;
 					break;
 				}
 				case ALLEGRO_KEY_W:
 				{
-					posicionY = posicionY - 30;
+					jugador.posicionY = jugador.posicionY - 30;
 					break;
 				}
 				case ALLEGRO_KEY_S:
 				{
-					posicionY = posicionY + 30;
+					jugador.posicionY = jugador.posicionY + 30;
 					break;
 				}
 				case ALLEGRO_KEY_ESCAPE:
