2cab064b7dddbfddb846452ff3b2f6fbc4f90355
bruno martinez flores <bruno.marflo@gmail.com>
2022-07-28 18:42:54 -0300

    logre implementar enemigos sin movimiento

--- Proyecto prog2/main.cpp
+++ Proyecto prog2/main.cpp
@@ -8,22 +8,29 @@
 #define alto 768
 #define M 64
 #define N 64
-
+#define MAX 5
 int puntaje(int mat[M][N]);//funcion para el puntaje
 
 struct posicion
 {
-	int posicionX = 0;
-	int posicionY = 0;
+	int posicionX=0;
+	int posicionY=0;
 };
 struct muro
 {
 	int posicionX[M][N];
 	int posicionY[M][N];
 };
+struct enemy
+{
+	int posicionX[MAX];
+	int posicionY[MAX];
+};
 
 typedef struct posicion movimiento;
+typedef struct enemy zombies;
 movimiento cargarmapa();
+zombies cargarenemigo();
 struct muro cargarpared();
 
 using namespace std;
@@ -37,6 +44,7 @@ ALLEGRO_EVENT_QUEUE* event_queue;
 int main()
 {
 	movimiento jugador;/*movimiento jugador en posiciones X e Y*/
+	zombies enemigo;
 	struct muro pared;
 	
 	int i=0, j=0;
@@ -55,39 +63,38 @@ int main()
 
 	event_queue = al_create_event_queue();
 	al_register_event_source(event_queue, al_get_keyboard_event_source());
-
+	
 	ALLEGRO_BITMAP* bloque = al_load_bitmap("datos/imagenes/pared.png");/*imagenes a utilizar*/
 	ALLEGRO_BITMAP* player = al_load_bitmap("datos/imagenes/personaje.png");
+	ALLEGRO_BITMAP* fondo = al_load_bitmap("datos/imagenes/cesped.jpg");
+	ALLEGRO_BITMAP* zombi = al_load_bitmap("datos/imagenes/fantasma.png");
 	/*ALLEGRO_BITMAP* menu_null1 = al_load_bitmap("imagenes/cara_benja.PNG"); */
 	
 	ALLEGRO_KEYBOARD_STATE* state{};
 
-	jugador = cargarmapa();//funcion para cargar mapa ¿se devuelven los datos?
-	
+	jugador = cargarmapa();//funcion para cargar mapa
+	enemigo = cargarenemigo();
 	pared = cargarpared();
 	
-	for (i = 0; i < M; i++)
-	{
-		for (j = 0; j < N; j++)
-		{
-			al_draw_bitmap(bloque, pared.posicionX[i][j], pared.posicionY[i][j], 0);
-			//printf("i = %d\t j = %d\n", i, j);
-			//printf("posicionX = %d\tposicionY = %d\n", pared.posicionX[i][j], pared.posicionY[i][j]);
-		}
-	}
 	while (true)
 	{
 		ALLEGRO_EVENT evento;
 		al_clear_to_color(al_map_rgb(0, 0, 0));
 
+		al_draw_bitmap(fondo, 0, 0, 0);
 		for (i = 0; i < M; i++)
 		{
 			for (j = 0; j < N; j++)
 			{
-				al_draw_bitmap(bloque, pared.posicionX[i][j], pared.posicionY[i][j], 0);
+				al_draw_bitmap(bloque, pared.posicionX[i][j], pared.posicionY[i][j], 0);//bloques
 			}
 		}
-		al_draw_bitmap(player, jugador.posicionX , jugador.posicionY , 0); /*personaje*/
+		for (i = 0; i < MAX; i++)
+		{
+			al_draw_bitmap(zombi, enemigo.posicionX[i], enemigo.posicionY[i], 0);//enemigo
+		}
+
+		al_draw_bitmap(player, jugador.posicionX, jugador.posicionY, 0); /*personaje*/
 
 		//printf("jugador\n");
 		//printf("posicionX = %d\tposicionY = %d\n", jugador.posicionX, jugador.posicionY);
@@ -132,9 +139,9 @@ int main()
 	}
 	return 0;
 }
-struct posicion cargarmapa()
+movimiento cargarmapa()
 {
-	struct posicion jugador;
+	movimiento jugador;
 	int i=0, j=0;
 
 	FILE* mapa;
@@ -166,6 +173,42 @@ struct posicion cargarmapa()
 	}
 	
 }
+
+zombies cargarenemigo()
+{
+	zombies enemigo;
+	int i = 0, j = 0,cont = 0;
+
+	FILE* mapa;
+
+	mapa = fopen("datos/mapas/mapa1.txt", "r");
+
+	for (i = 0; i < M; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+			fscanf(mapa, "%c", &mat[i][j]);
+		}
+		//fscanf(mapa, "\n");
+	}
+	fclose(mapa);
+	for (i = 0; i < M; i++)
+	{
+		for (j = 0; j < N; j++)
+		{
+
+			//printf("%c", mat[i][j]);
+			if (mat[i][j] == 'e' && cont < MAX)
+			{
+				enemigo.posicionX[cont] = i * 16;
+				enemigo.posicionY[cont] = j * 12;
+				cont++;
+			}
+		}
+	}
+	return enemigo;
+}
+
 
 struct muro cargarpared()
 {
@@ -203,4 +246,6 @@ struct muro cargarpared()
 	} 
 	return pared;
 }
+
+
 
