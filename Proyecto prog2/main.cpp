3a6998ef602c35fa19fd3b6b255c09b92cfee30e
bruno martinez flores <bruno.marflo@gmail.com>
2022-08-29 02:16:10 -0300

    implemente una pantalla para el ranking y arregle algo del movimiento de los enemigos

--- Proyecto prog2/main.cpp
+++ Proyecto prog2/main.cpp
@@ -20,9 +20,9 @@
 
 int puntaje(int puntos,int valor);//funcion para el puntaje
 int vidas(int corazones);
-int menuflg(ALLEGRO_BITMAP*inicio[5], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int bandera);
-int gameover(ALLEGRO_COLOR[3], ALLEGRO_FONT* font[2], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos);
-int rnk(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[2], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos, int bandera);
+int menuflg(ALLEGRO_BITMAP*inicio[6], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int bandera);
+int gameover(ALLEGRO_COLOR[3], ALLEGRO_FONT* font[3], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos);
+
 
 struct player
 {
@@ -60,13 +60,10 @@ typedef struct player jugador;
 typedef struct enemy zombies ;
 
 //ctr+k->c
-//implementar cambio de mapas
-//simbolo dentro de mapa que sea una puerta hacia otro nivel 
-//solo aparece si puntaje > ciertos puntos 
-//crear bandera para crear/cambiar mapas
-//an
 void setmapa(const char*file_name);
 int leerrnk(usuario rnk[], int id);
+int verrnk(ALLEGRO_BITMAP* inicio[6],ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[3], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, usuario rnk[], int id);
+int guardarrnk(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[3], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos, int bandera);
 int personalidad(zombies enemigo[], int i);
 jugador setjugador();
 elementos cargarelementos(int index);
@@ -142,13 +139,14 @@ int main()
 	ALLEGRO_BITMAP* opciones = al_load_bitmap("datos/imagenes/opciones.png");//2
 	ALLEGRO_BITMAP* ranking = al_load_bitmap("datos/imagenes/ranking.png");//3
 	ALLEGRO_BITMAP* salir = al_load_bitmap("datos/imagenes/salir.png");//4
-	ALLEGRO_BITMAP* inicio[5] = { al_load_bitmap("datos/imagenes/menu.png"),al_load_bitmap("datos/imagenes/jugar.png"),al_load_bitmap("datos/imagenes/opciones.png"),al_load_bitmap("datos/imagenes/ranking.png"),al_load_bitmap("datos/imagenes/salir.png") };
+	ALLEGRO_BITMAP* puntajes = al_load_bitmap("datos/imagenes/puntajes.png");//5
+	ALLEGRO_BITMAP* inicio[6] = { al_load_bitmap("datos/imagenes/menu.png"),al_load_bitmap("datos/imagenes/jugar.png"),al_load_bitmap("datos/imagenes/opciones.png"),al_load_bitmap("datos/imagenes/ranking.png"),al_load_bitmap("datos/imagenes/salir.png"), al_load_bitmap("datos/imagenes/puntajes.png") };
 	
 	//////////////////////////////////////////////////////       LETRAS     ////////////////////////////////////////////////////////////////////////
 	ALLEGRO_FONT* letras = al_load_font("datos/fuentes/AldotheApache.ttf", 50, 0);
 	ALLEGRO_FONT* letras1 = al_load_font("datos/fuentes/AldotheApache.ttf", 20, 0);
 	ALLEGRO_FONT* letras3 = al_load_font("datos/fuentes/AldotheApache.ttf", 40, 0);
-	ALLEGRO_FONT* font[2] = { al_load_font("datos/fuentes/AldotheApache.ttf", 50, 0) ,al_load_font("datos/fuentes/AldotheApache.ttf", 20, 0) };
+	ALLEGRO_FONT* font[3] = { al_load_font("datos/fuentes/AldotheApache.ttf", 50, 0) ,al_load_font("datos/fuentes/AldotheApache.ttf", 20, 0),al_load_font("datos/fuentes/AldotheApache.ttf", 40, 0) };
 
 	////////////////////////////////////////////////////////     COLORES     //////////////////////////////////////////////////////////////////////////////////////////
 	ALLEGRO_COLOR negro = al_map_rgb(0, 0, 0);
@@ -179,7 +177,7 @@ int main()
 	al_attach_sample_instance_to_mixer(songinstance, al_get_default_mixer());
 
 	setmapa("datos/mapas/mapa1.txt");//funcion para cargar mapa
-	//id = leerrnk(rank, id);
+	leerrnk(rank, id);
 
 	jugador = setjugador();
 	x = jugador.posicionX;
@@ -188,7 +186,6 @@ int main()
 	for (i = 0; i < MAX; i++)
 	{
 		enemigo[i] = cargarenemigo(i);
-
 	}
 	
 	for (i = 0; i < MAXELEM; i++)
@@ -215,21 +212,44 @@ int main()
 		}
 		if (bandera == 0)//menu 
 		{
+			al_show_mouse_cursor(ventana);
 			bandera = menuflg(inicio, evento, event_queue, bandera);
 			if (bandera == 1)
 			{
 				al_hide_mouse_cursor(ventana);
 			}
+			cont = 1;
 		}
 		if (bandera == 1)//juego
 		{
 			//al_play_sample_instance(songinstance);
 
 			al_draw_bitmap(fondo, 0, 0, 0);
+			printf("cont=%d\n", cont);
+
+			for (i = 0; i < 2; i++)
+			{
+				if (nivel >= usuario.puntaje);
+				{
+					al_draw_bitmap_region(entrada, 8, 12, 8, 14, pared.posicionpuertaX[i] * 16, pared.posicionpuertaY[i] * 12, 0);
+				}
+				if (nivel < usuario.puntaje)
+				{
+					al_draw_bitmap_region(entrada, 18, 12, 11, 15, pared.posicionpuertaX[i] * 16, pared.posicionpuertaY[i] * 12, 0);
+
+					if (jugador.posicionX == pared.posicionpuertaX[i] && jugador.posicionY == pared.posicionpuertaY[i])
+					{
+						stge_nivel = 1;
+						nivel = nivel + 1;
+
+					}
+				}
+			}
 
 			if (stge_nivel == 1)
 			{
 				cont++;
+
 				switch (cont)
 				{
 				case 2:
@@ -266,7 +286,6 @@ int main()
 				default:
 					break;
 				}
-
 				jugador = setjugador();
 				for (i = 0; i < MAX; i++)
 				{
@@ -280,24 +299,6 @@ int main()
 				stge_nivel = 0;
 			}
 
-			for (i = 0; i < 2; i++)
-			{
-				if (nivel >= usuario.puntaje);
-				{
-					al_draw_bitmap_region(entrada, 8, 12, 8, 14, pared.posicionpuertaX[i] * 16, pared.posicionpuertaY[i] * 12, 0);
-				}
-				if (nivel < usuario.puntaje)
-				{
-					al_draw_bitmap_region(entrada, 18, 12, 11, 15, pared.posicionpuertaX[i] * 16, pared.posicionpuertaY[i] * 12, 0);
-
-					if (jugador.posicionX == pared.posicionpuertaX[i] && jugador.posicionY == pared.posicionpuertaY[i])
-					{
-						stge_nivel = 1;
-						nivel = nivel + 1;
-						
-					}
-				}
-			}
 
 			for (i = 0; i < M; i++)
 			{
@@ -309,7 +310,6 @@ int main()
 
 			for (i = 0; i < MAX; i++)
 			{
-				//al_draw_bitmap(zombi, enemigo[i].posicionX * 16, enemigo[i].posicionY * 12, 0);//enemigo
 				al_draw_bitmap_region(zombi, 456, 64, 15, 15, enemigo[i].posicionX * 16, enemigo[i].posicionY * 12, 0);
 			}
 
@@ -406,10 +406,9 @@ int main()
 			{
 				for (i = 0; i < MAX; i++)
 				{
-					enemigo[0].personalidad = 1;
 					switch (enemigo[i].personalidad)
 					{
-						//aleatorio
+					//aleatorio
 					case 0:
 					{
 						azar = rand()%4;
@@ -457,18 +456,29 @@ int main()
 					//buscar
 					case 1:
 					{
+						for (j = 0; j < MAX; j++)
+						{
+							if (enemigo[i].posicionX * 16 == enemigo[j].posicionX * 16 && enemigo[i].posicionY * 12 == enemigo[j].posicionY * 12 && i!=j)//cuando el jugador choca con un enemigo
+							{
+								enemigo[j].personalidad = 0;
+							}
+						}
+
 						if (enemigo[i].posicionX > jugador.posicionX && mat[enemigo[i].posicionX - 1][enemigo[i].posicionY] != 'x')
 						{
 							enemigo[i].posicionX--;
 						}
+
 						else if (enemigo[i].posicionX < jugador.posicionX && mat[enemigo[i].posicionX + 1][enemigo[i].posicionY] != 'x')
 						{
 							enemigo[i].posicionX++;
 						}
+
 						if (enemigo[i].posicionY > jugador.posicionY && mat[enemigo[i].posicionX][enemigo[i].posicionY-1] != 'x')
 						{
 							enemigo[i].posicionY--;
 						}
+
 						else if (enemigo[i].posicionY < jugador.posicionY && mat[enemigo[i].posicionX][enemigo[i].posicionY + 1] != 'x')
 						{
 							enemigo[i].posicionY++;
@@ -591,9 +601,9 @@ int main()
 				}
 			}
 		}
-		if (bandera == 2)//ranking
+		if (bandera == 2)//nombre
 		{
-			bandera = rnk(colors,font,evento,event_queue,usuario.puntaje,bandera);
+			bandera = guardarrnk(colors,font,evento,event_queue,usuario.puntaje,bandera);
 		}
 		if (bandera == 3)//game over
 		{
@@ -603,6 +613,10 @@ int main()
 			if (gmrv == 2)
 				bandera = 0;
 		}
+		if (bandera == 4)/*ranking*/
+		{
+			bandera = verrnk(inicio,colors, font, evento, event_queue, rank, id);
+		}
 		al_flip_display();
 	}
 	
@@ -611,7 +625,7 @@ int main()
 void setmapa(const char* file_name)
 {
 	int i = 0, j = 0;
-	//char aux[;
+	//char aux;
 	FILE* mapa;
 
 	mapa = fopen(file_name, "r");
@@ -622,7 +636,12 @@ void setmapa(const char* file_name)
 		{
 			fscanf(mapa, "%c", &mat[j][i]);
 		}
-		//fscanf(mapa, "%c",aux);
+		//fscanf(mapa, "%c",&aux);
+	}
+	if (mapa == NULL)
+	{
+		printf("no se abrio el archivo");
+		exit(-1);
 	}
 	fclose(mapa);
 }
@@ -672,12 +691,6 @@ zombies cargarenemigo(int index)
 		}
 	}
 	return enemigo[index];
-}
-
-int personalidad(zombies enemigos[], int i)
-{
-
-	return i;
 }
 
 elementos cargarelementos(int index)
@@ -745,7 +758,7 @@ int vidas(int corazones)
 	return  corazones;
 }
 
-int menuflg(ALLEGRO_BITMAP* inicio[5], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int bandera)
+int menuflg(ALLEGRO_BITMAP* inicio[6], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int bandera)
 {
 	al_draw_bitmap(inicio[0], 0, 0, 0);
 
@@ -810,7 +823,7 @@ int menuflg(ALLEGRO_BITMAP* inicio[5], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE
 			}
 			if (mouseX < 325 && mouseX > 22 && mouseY < 663 && mouseY > 602)//ranking
 			{
-				bandera = -1;
+				bandera = 4;
 			}
 			break;
 		}
@@ -821,12 +834,12 @@ int menuflg(ALLEGRO_BITMAP* inicio[5], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE
 	return bandera;
 }
 
-int gameover(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[2], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos)
+int gameover(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[3], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos)
 {
 	al_draw_textf(font[0], colors[2], ancho / 2 - 16 * 14, alto / 4 + 12 * 3, NULL, "tus puntos fueron de :%d", puntos);
 	al_draw_text(font[0], colors[1], ancho / 2 - 16 * 6, alto / 5, NULL, "GAME OVER");
 	al_draw_text(font[1], colors[1], ancho / 2 - 16 * 6, alto / 2 + 12 * 6, NULL, "presione escape para salir");
-	al_draw_text(font[1], colors[1], ancho / 2 - 16 * 6, alto / 2 + 12 * 8, NULL, "presione enter para volver al menu");
+	al_draw_text(font[1], colors[1], ancho / 2 - 16 * 6, alto / 2 + 12 * 8, NULL, "presione tab para volver al menu");
 	if (evento.type == ALLEGRO_EVENT_KEY_DOWN)
 	{
 		switch (evento.keyboard.keycode)/*hacer una accion para cada tecla pulsada*/
@@ -835,7 +848,7 @@ int gameover(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[2], ALLEGRO_EVENT event
 		{
 			return 1;
 		}
-		case ALLEGRO_KEY_ENTER:
+		case ALLEGRO_KEY_TAB:
 		{
 			return 2;
 		}
@@ -849,7 +862,7 @@ int gameover(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[2], ALLEGRO_EVENT event
 int leerrnk(usuario rnk[], int id)
 {
 	FILE* ranking;
-	ranking = fopen("datos/ranking/puntaje.txt", "r");
+	ranking = fopen("datos/ranking/rank.txt", "r");
 	if (ranking == NULL)
 	{
 		printf("no se abrio el archivo");
@@ -857,46 +870,76 @@ int leerrnk(usuario rnk[], int id)
 	}
 	while (!feof(ranking))
 	{
-		fscanf(ranking, "%s", rank[id].nombre);
-		fscanf(ranking, "%s", rank[id].puntaje);
+		fscanf(ranking, "%s", &rank[id].nombre);
+		fscanf(ranking, "%d", &rank[id].puntaje);
 		id++;
 	}
 	fclose(ranking);
 	return id;
 }
 
-int rnk(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[2], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos, int bandera)
+int verrnk(ALLEGRO_BITMAP* inicio[6],ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[3], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue,usuario rnk[], int id)
+{
+	int i=1,j=0;
+	al_draw_bitmap(inicio[5], 0, 0, 0);
+	for (id = 0; id < 9; id++)
+	{
+		j++;
+		printf("%s\t%d\n", rank[id].nombre, rank[id].puntaje);
+
+		al_draw_text(font[0], colors[1],  16 * 8, 12 * 8, NULL, "presione escape para volver al menu");
+		al_draw_textf(font[2], colors[1], ancho / 2 - 16 * 8, alto / 4 + 12 * i, NULL, "%d.-%s\t%d",j,rank[id].nombre,rank[id].puntaje);
+		i = i + 4;
+	}
+	if (evento.type == ALLEGRO_EVENT_KEY_DOWN)
+	{
+		switch (evento.keyboard.keycode)/*hacer una accion para cada tecla pulsada*/
+		{
+		case ALLEGRO_KEY_ESCAPE:
+		{
+			return 0;
+		}
+		default:
+			break;
+		}
+	}
+	return 4;
+}
+
+int guardarrnk(ALLEGRO_COLOR colors[3], ALLEGRO_FONT* font[3], ALLEGRO_EVENT evento, ALLEGRO_EVENT_QUEUE* event_queue, int puntos, int bandera)
 {
 	int i = 0, espacio = ancho / 2 - 16 * 6;
-	char name[30]="pinono\n";
-	usuario jugador;
-
 	FILE* ranking;
-	ALLEGRO_FILE* rank;
+	ranking = fopen("datos/ranking/rank.txt", "w");//escritura o creacion del ranking en caso de que no exista
+	printf("ciclo\n");
 
-	rank = al_fopen("datos/ranking/puntajeallegro.txt", "w+");
-	ranking = fopen("datos/ranking/puntaje.txt", "w+");//escritura o creacion del ranking en caso de que no exista
+	if (ranking == NULL)
+	{
+		printf("no se abrio el archivo");
+		exit(-1);
+	}
 
 	al_clear_to_color(colors[0]);
-	jugador.puntaje = puntos;
-	//printf("%d", jugador.puntaje);
 	al_draw_text(font[1], colors[1], 16*15, alto / 2 + 12 * 10, NULL, "ingrese nombre usuario: ");
-	
-	//while (i < 2)
-	//{
-	//	scanf("%s",jugador.nombre);
-	//	//printf("i = %d\n", i);
+	if (feof(ranking))
+	{
+		printf("hola");
+		fprintf(ranking, "%s  %d", rank[0].nombre, puntos);
+	}
 
-	//	//strcpy(jugador.nombre, name);
-
-	//	fprintf(ranking, "%c-%d\n", jugador.nombre, puntos);//c++
-
-	//	al_fputs(rank, jugador.nombre);//allegro
-
-	//	al_fputc(rank, jugador.puntaje);
-	//	i = i + 1;
-	//}
-	al_fclose(rank);
 	fclose(ranking);
+	if (evento.type == ALLEGRO_EVENT_KEY_DOWN)
+	{
+		switch (evento.keyboard.keycode)/*hacer una accion para cada tecla pulsada*/
+		{
+		case ALLEGRO_KEY_1:
+		{
+			printf("paso\n");
+			return 3;
+		}
+		default:
+			break;
+		}
+	}
 	return bandera;
 }
